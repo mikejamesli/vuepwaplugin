3a040f9670f6ca1d3579d92027dca070b7729cc3
Michael James <lilesoftware@gmail.com>
2018-11-13 09:05:06 +0000

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-     Does this update in iOS?
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
+  YES
   </div>
 </template>
 
